966babac4ea28410b80a52d46d095656c4bfb8f8
Hasan <programmermdhasan@gmail.com>
2023-04-02 11:00:34 +0600

    use cors

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 const app = express();
 const PORT = 3001;
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-};
+// const corsOptions = {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// };
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(cors());
 // create schema
 const usersSchema = new mongoose.Schema({
     name: {
